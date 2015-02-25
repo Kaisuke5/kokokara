647d7b26cffff5d9d57bbc3fe9bc4b189eaba87f
kaishunsuke <kaisuke5.roy7@gmail.com>
2015-02-25 14:40:51 +0900

    sql edit

--- app/Model/kokokara.sql
+++ app/Model/kokokara.sql
@@ -2,6 +2,31 @@ create table students(
 
   id int not null,
 
-kaikaikai
+
+)
+
+
+create table events(
+id int auto_increment,primary key,
+state int,
+common1 varchar(50),
+common2 varchar(50),
+
+)
+
+
+create table eventoriginal1(
+id int auto_increment,primary key,
+eventid int,
+originl1 varchar(50),
+
+)
+
+
+create table eventoriginal2(
+id int auto_increment,primary key,
+eventid int,
+originl1 varchar(50),
+originl2 varchar(50),
 
 )
