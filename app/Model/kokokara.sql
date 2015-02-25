286cdca9763bc10d2642a9a816e2d941807f867c
mark9211 <mark.sato1111@gmail.com>
2015-02-25 14:50:19 +0900

    sql student

--- app/Model/kokokara.sql
+++ app/Model/kokokara.sql
@@ -1,12 +1,16 @@
+# Model
 create table students(
   id int not null primary key auto_increment,
   name VARCHAR (255),
   password VARCHAR (255),
   created datetime DEFAULT NULL ,
   modified datetime DEFAULT NULL
-)
+);
 
-
+# Test
+INSERT INTO students(name, password, created, modified) VALUES ('user1', 'password1', now(), now());
+INSERT INTO students(name, password, created, modified) VALUES ('user2', 'password2', now(), now());
+INSERT INTO students(name, password, created, modified) VALUES ('user3', 'password3', now(), now());
 
 
 create table events(
@@ -15,7 +19,7 @@ state int,
 common1 varchar(50),
 common2 varchar(50),
 
-)
+);
 
 
 create table eventoriginal1(
@@ -23,7 +27,7 @@ id int auto_increment,primary key,
 eventid int,
 originl1 varchar(50),
 
-)
+);
 
 
 create table eventoriginal2(
@@ -32,5 +36,5 @@ eventid int,
 originl1 varchar(50),
 originl2 varchar(50),
 
-)
+);
 
