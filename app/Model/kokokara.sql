064e419eb664c1fdda659d8fb52448e72957e3ff
mark9211 <mark.sato1111@gmail.com>
2015-02-25 15:05:44 +0900

    unko

--- app/Model/kokokara.sql
+++ app/Model/kokokara.sql
@@ -14,27 +14,27 @@ INSERT INTO students(name, password, created, modified) VALUES ('user3', 'passwo
 
 
 create table events(
-id int auto_increment,primary key,
-state int,
-common1 varchar(50),
-common2 varchar(50),
+  id int auto_increment,primary key,
+  state int,
+  common1 varchar(50),
+  common2 varchar(50)
 
 );
 
 
 create table eventoriginal1(
-id int auto_increment,primary key,
-eventid int,
-originl1 varchar(50),
+  id int auto_increment,primary key,
+  eventid int,
+  originl1 varchar(50)
 
 );
 
 
 create table eventoriginal2(
-id int auto_increment,primary key,
-eventid int,
-originl1 varchar(50),
-originl2 varchar(50),
+  id int auto_increment,primary key,
+  eventid int,
+  originl1 varchar(50),
+  originl2 varchar(50)
 
 );
 
